63b6aa2109c4688ee77354083b1fdf9bad707368
annedawson <anne.dawson@gmail.com>
2021-05-10 13:20:14 -0700

    Commit - Monday 10th May, 2021 https://codinginflow.com/tutorials/android/simple-recyclerview-kotlin/part-3-insert-remove-items After completing Part 3

--- app/src/main/java/net/annedawson/recyclerviewexample/ExampleAdapter.kt
+++ app/src/main/java/net/annedawson/recyclerviewexample/ExampleAdapter.kt
@@ -1,5 +1,6 @@
 package net.annedawson.recyclerviewexample
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,30 +19,46 @@ import kotlinx.android.synthetic.main.example_item.view.*
 // kotlin android extensions is deprecated - use Jetpack view binding instead
 
 class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>(){
+    // must implement these 3 member methods:  press Ctrl I at this location
 
+    // called a few times, once for each item that appears on the screen
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
            parent, false)
-
+        // the line above is a bit complex but you don't need to understand it, just use it as is
+        // and all should be well. parent is the RecyclerView this viewholder will be placed in
+        // LayoutInflater converts an xml layout into a view object
+        // context is the main_activity
+        // don't worry about understanding the line above. Use as is.
         return ExampleViewHolder(itemView)
     }
 
+    // called over and over again, either when we scroll items on the screen or update them
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
         val currentItem = exampleList[position]
 
         holder.imageView.setImageResource(currentItem.imageResource)
-        holder.textView1.text = currentItem.text1
-        holder.textView2.text = currentItem.text2
-    }
+        holder.textView1.text = currentItem.text1   // Ctrl D to duplicate a line
+        holder.textView2.text = currentItem.text2   // kotlin's property syntax
+        // the above is equivalent to
+        // holder.itemView.text_view_1.text = currentItem.text2
+        // it will work but it uses findViewById which we want to avoid as this method
+        // is called over and over again
+
+           }
 
     override fun getItemCount() = exampleList.size
 
-
+    // nested class
     class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView: ImageView = itemView.image_view
-        val textView1: TextView = itemView.text_view_1
-        val textView2: TextView = itemView.text_view_2
-
+        // a ViewHolder represents a single row in our list
+        // references to each view in each row.
+        // cached references in ViewHolder and ViewHolder is recycled
+        // three properties:
+        val imageView: ImageView = itemView.image_view // image_view is a synthetic property
+        val textView1: TextView = itemView.text_view_1 // text_view_1 is a synthetic property
+        val textView2: TextView = itemView.text_view_2 // text_view_2 is a synthetic property
+        // the above is equivalent to itemView.findViewById(R.id.text_view_2)
     }
 
 
--- app/src/main/java/net/annedawson/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/net/annedawson/recyclerviewexample/MainActivity.kt
@@ -2,21 +2,47 @@ package net.annedawson.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val exampleList = generateDummyList(500)  // a property of this class private to the class
+    private val adapter = ExampleAdapter(exampleList)  // a ref to list - call by reference - same list as in adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateDummyList(500)
-        recycler_view.adapter = ExampleAdapter(exampleList)
-        recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.setHasFixedSize(true)
+        //recycler view with synthetic property - deprecated
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = LinearLayoutManager(this) // this is the current activity
+        recycler_view.setHasFixedSize(true)  // just an optimisation if we know if the RecyclerView
+                                             // has fixed width and height
     }
 
-    private fun generateDummyList(size: Int): List<ExampleItem> {
+    fun insertItem(view: View) { // must match name on onclick in xml and needs view: View but you can ignore it
+        val index = Random.nextInt(8)  // 0 to 7 random number
+        val newItem = ExampleItem(
+            R.drawable.ic_android,
+            "New item at position $index",
+            "Line 2"
+        )
+        exampleList.add(index, newItem)
+        adapter.notifyItemInserted(index) // refreshing list in RecycleView
+    }
+
+
+    fun removeItem(view: View) {
+        val index = Random.nextInt(8)
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index) // refreshing list in RecycleView
+    }
+
+
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {  // ArrayList is modifyable
         val list = ArrayList<ExampleItem>()
         for (i in 0 until size) {
             val drawable = when (i % 3) {
